4b85706d20aa1dd4798d163080cadb501212f715
pjzabala <pgzabala.dmci@gmail.com>
2025-09-05 08:56:26 +0800

    update abpp systems

--- ABPP/javascript/config.js
+++ ABPP/javascript/config.js
@@ -3,7 +3,7 @@ const gvizURL =
 
 const systemGroups = {
   equipments: [],
-  cs: ["Cooling Water System"],
+  cs: ["Cooling Water System", "Engine"],
   es: ["Exhaust System", "Steam"],
   fos1: ["Fuel Oil System 1",],
   fos2: ["Fuel Oil System 2"],
@@ -19,6 +19,8 @@ const positionMaps = {
     "HT Water Preheater 2": { x: 550, y: 565 },
     "Nozzle Cooling Water Pump 1": { x: 392, y: 170 },
     "Nozzle Cooling Water Pump 2": { x: 392, y: 545 },
+    "MAN 1": { x: 217, y: 195 },
+    "MAN 2": { x: 217, y: 575 },
   },
   es: {
     "Exhaust Gas Boiler 1": { x: 533, y: 125 },
@@ -26,12 +28,18 @@ const positionMaps = {
   },
   fos1: {
     "LFO Supplying Pump": { x: 385, y: 550 },
+    "LFO Unloading Pump Module": { x: 410, y: 280 },
+    "LFO Day Tank": { x: 730, y: 500 },
+    "LFO Storage Tank": { x: 875, y: 280 },
   },
   fos2: {
     "HFO Supplying Pump": { x: 305, y: 420 },
     "HFO Separator": { x: 315, y: 647 },
     "HFO Day Tank": { x: 620, y: 510 },
-    "HFO Buffer Tank": { x: 792, y: 510 }
+    "HFO Buffer Tank": { x: 792, y: 510 },
+    "HFO Storage Tank": { x: 410, y: 167 },
+    "HFO Unloading Pump": { x: 180, y: 162 },
+    "HFO Transfer Pump": { x: 530, y: 162 },
   },
   fos3: {
     "Fuel Oil Circulating Booster Pump 1": { x: 632, y: 190 },
@@ -43,5 +51,9 @@ const positionMaps = {
     "Oil Mist Separator 2": { x: 693, y: 115 },
     "Lube Oil Maintenance Tank": { x: 90, y: 360 },
     "Used Lube Oil Tank": { x: 90, y: 575 },
+    "Fuel Oil Purifier 1": { x: 490, y: 600 },
+    "Fuel Oil Purifier 2": { x: 895, y: 600 },
+    "Lube Oil Service Tank 1": { x: 280, y: 437 },
+    "Lube Oil Service Tank 2": { x: 740, y: 437 },
   },
 };
